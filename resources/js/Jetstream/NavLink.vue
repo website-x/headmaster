7e3781e76dd8941b3eb39c6476e99e278029eaf4
Shreyansh Panchal <sunpan835@gmail.com>
2020-10-13 14:04:47 +0530

    Text size changed

--- resources/js/Jetstream/NavLink.vue
+++ resources/js/Jetstream/NavLink.vue
@@ -11,8 +11,8 @@
         computed: {
             classes() {
                 return this.active
-                            ? 'inline-flex items-center px-1 pt-1 border-b-2 border-green-300 text-sm font-medium leading-5 text-white focus:outline-none focus:border-indigo-700 transition duration-150 ease-in-out'
-                            : 'inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-white hover:text-green-300 hover:border-gray-300 focus:outline-none focus:border-gray-300 transition duration-150 ease-in-out'
+                            ? 'inline-flex items-center px-1 pt-1 border-b-2 border-green-300 font-medium leading-5 text-white focus:outline-none focus:border-indigo-700 transition duration-150 ease-in-out'
+                            : 'inline-flex items-center px-1 pt-1 border-b-2 border-transparent font-medium leading-5 text-white hover:text-green-300 hover:border-gray-300 focus:outline-none focus:border-gray-300 transition duration-150 ease-in-out'
             }
         }
     }
